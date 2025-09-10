6df11bcdf376784f7002b30875cf8ad9c5ecff79
Wyatt Johnson <wyatt@Hel.local>
2025-09-10 14:59:06 -0700

    flake structure finally!

--- flake.nix
+++ flake.nix
@@ -9,20 +9,49 @@
     };
 
     outputs = { self, nixpkgs, home-manager,  ... }: {
-        nixosConfigurations.jormungandr = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./configuration.nix
-                home-manager.nixosModules.home-manager
-                {
-                    home-manager = {
+        # nixosConfigurations.jormungandr = nixpkgs.lib.nixosSystem {
+        #     system = "x86_64-linux";
+        #     modules = [
+        #         ./host/configuration.nix
+        #         home-manager.nixosModules.home-manager
+        #         {
+        #             home-manager = {
+        #                 useGlobalPkgs = true;
+        #                 useUserPackages = true;
+        #                 users.wyatt = import ./home.nix;
+        #                 backupFileExtension = "backup";
+        #             };
+        #         }
+        #     ];
+        # };
+        nixosConfigurations = {
+            fenrir = nixpkgs.lib.nixos {
+                system = "x86_64-linux"
+                modules = [
+                    ./host/fenrir/configuration.nix
+                    home-manager.nixosModules.home-manager
+                    {
                         useGlobalPkgs = true;
                         useUserPackages = true;
-                        users.wyatt = import ./home.nix;
-                        backupFileExtension = "backup";
+                        users.wyatt = import ./host/fenrir/home.nix
                     };
-                }
-            ];
+                ];
+            };
+            jormungandr = nixpkgs.lib,nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                    ./host/jormungandr/configuration.nix
+                    home-manager.nixosModules.home-manager
+                    {
+                        home-manager = {
+                            useGlobalPkgs = true;
+                            useUserPackages = true;
+                            users.wyatt = import ./host/jormungandr/home.nix;
+                            backupFileExtension = "backup";
+                        };
+                    };
+                ];
+            };
         };
     };
 }
