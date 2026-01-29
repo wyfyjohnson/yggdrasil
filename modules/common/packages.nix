581db8c43d3a3759000b459bddd9f07bb96b5beb
Wyatt <johnson.wyatt.n@gmail.com>
2026-01-29 12:43:29 -0800

    trying to hastely add all the packages that were removed

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   huginn,
+  wfetch,
   ...
 }: {
   environment.systemPackages = with pkgs; [
@@ -10,21 +11,62 @@
     git
     nil
 
-    # Terminal utilities
+    # Terminal & Shell
+    bat
     bottom
+    btop-rocm
+    cava
     curl
     eza
     fastfetch
+    fzf
     hyfetch
     krabby
     onefetch
+    starship
     tut
     wget
     yazi
 
+    # Editors
+    helix
+
+    # Browsers & Communication
+    discord
+    firefox
+    signal-desktop
+    vivaldi
+    webcord
+
     # Media and utilities
+    ascii-image-converter
+    imagemagick
     kew
+    mpv
+    wf-recorder
     yt-dlp
+
+    # Wayland/Hyprland tools
+    cliphist
+    dunst
+    hyprlock
+    hyprls
+    hyprpicker
+    hyprshot
+    swww
+    waybar
+    wl-clipboard
+
+    # Desktop tools
+    kitty
+    libreoffice
+    mullvad-vpn
+    nitrogen
+    pamixer
+    pavucontrol
+    picom
+    pinentry-curses
+    playerctl
 
     # Programming languages
     nodejs
@@ -38,8 +80,13 @@
     rust-analyzer
     vscode-langservers-extracted
 
+    # Shell
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+
     # Custom packages
     (pkgs.callPackage ../../yggdrasil.nix {})
     huginn.packages.${pkgs.system}.default
+    wfetch.packages.${pkgs.system}.default
   ];
 }
